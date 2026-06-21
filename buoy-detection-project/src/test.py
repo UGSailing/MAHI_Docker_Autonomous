913f00998fd600dcc3975358328f2bfac8b6f003
Robin Aerts <nibor.aerts@gmail.com>
2026-06-22 01:27:54 +0200

    changed buoy

--- buoy-detection-project/src/test.py
+++ buoy-detection-project/src/test.py
@@ -11,7 +11,7 @@ from post_mqtt import *
 
 
 def main() -> None:
-    publish_detection_coordinates([{"buoy": "1", "latitude": 51.08237244887396, "longitude": 2.588432716735549}, {"buoy": "2", "latitude": 51.08243266810585, "longitude": 2.5884415111765375}])
+    publish_detection_coordinates([{"buoy": "1", "latitude": 51.082319260897066, "longitude": 2.5878400403344624}, {"buoy": "2", "latitude": 51.08243266810585, "longitude": 2.5884415111765375}])
     
     
 if __name__ == "__main__":
