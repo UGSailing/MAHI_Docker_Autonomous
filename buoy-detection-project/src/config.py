93ef14586b8f87c3e230e5d1f33649acd2cf380a
Robin Aerts <nibor.aerts@gmail.com>
2026-06-25 12:04:20 +0200

    speed

--- buoy-detection-project/src/config.py
+++ buoy-detection-project/src/config.py
@@ -8,4 +8,4 @@ APRIORI_BUOYLIST = [
     [(51.14339735730541, 2.7468965058450774)],   # buoy 0 — replace with real a-priori GPS coords
     [(51.14344451713093, 2.7475114395335662)],   # buoy 1 — replace with real a-priori GPS coords
 ]
-SPEED = 2
+SPEED = 3
