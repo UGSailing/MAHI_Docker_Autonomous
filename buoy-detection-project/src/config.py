5c3f876c9b49c3370ecf148d420b7c1cf957aabc
Robin Aerts <nibor.aerts@gmail.com>
2026-06-25 14:57:06 +0200

    test params

--- buoy-detection-project/src/config.py
+++ buoy-detection-project/src/config.py
@@ -3,10 +3,10 @@ MARGE = 4
 STATE_TRANS_DIST = MARGE * 1.5
 BUOY_MATCH_DISTANCE = MARGE + 1.5
 N_ARC_PTS = 12           # waypoints voor de 180° bocht om B2
-N_SLALOM_PTS = 12        # waypoints per slalom-been (S-curve)
+N_SLALOM_PTS = 20        # waypoints per slalom-been (S-curve)
 APRIORI_BUOYLIST = [
     [(51.14339735730541, 2.7468965058450774)],   # buoy 0 — replace with real a-priori GPS coords
     [(51.14344451713093, 2.7475114395335662)],   # buoy 1 — replace with real a-priori GPS coords
 ]
 SPEED = 6
-TILT = 0
+TILT = 0.5
