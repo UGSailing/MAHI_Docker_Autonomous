9ac612b5e88044d9e0a81654dd80ae9397c7a3c6
Simeon Demeester <simeon.demeester@ugent>
2026-06-30 12:30:22 +0200

    added startup sequence

---
+++ startup_race/src/check_camera.py
@@ -0,0 +1,3 @@
+def check() -> bool:
+    print("Not implemented yet but the code continues in order to check the statemachine")
+    return True
---
+++ startup_race/src/execute_race.py
@@ -0,0 +1,6 @@
+def run(mission_id: int) -> bool:
+    print(
+        f"Not implemented yet but the code continues in order to check the statemachine "
+        f"(mission_id={mission_id})"
+    )
+    return True
